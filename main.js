f8dd649c188bc8d80da48e692eef1b5d356aa0aa
fkangcd <1353829153@qq.com>
2022-04-25 19:02:48 +0800

    完成home首页开发

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 import App from './App'
-
 // #ifndef VUE3
 import Vue from 'vue'
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+$http.baseUrl = 'http://api-hmugo-web.itheima.net'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
