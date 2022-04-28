eeef5e6e08b6eeb862cd72f37e62299d8145ae1a
fkangcd <1353829153@qq.com>
2022-04-28 13:53:40 +0800

    完成搜索功能开发

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
 import { $http } from '@escook/request-miniprogram'
 
 uni.$http = $http
